5f3b535b8ac9ba87d3d5afda8724e54fbe0fc729
CNadjim <c.nadjim@gmail.com>
2019-03-17 17:45:19 +0100

    update readme

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  backUrl: 'http://137.74.44.48:8060',
-  frontUrl: 'http://137.74.44.48:8070'
+  backUrl: 'http://bettorleague.ovh:8060',
+  frontUrl: 'http://bettorleague.ovh:8070'
 };
