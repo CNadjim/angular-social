0c78b419a6e869073ec7931225fa69ce2cfb8112
CNadjim <c.nadjim@gmail.com>
2019-03-24 20:21:20 +0100

    treafik

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backUrl: 'http://bettorleague.ovh:8060',
-  frontUrl: 'http://web.spring-social.ovh',
+  backUrl: 'https://api.spring-social.ovh',
+  frontUrl: 'https://web.spring-social.ovh',
   github: 'https://github.com/CNadjim/angular-social'
 };
